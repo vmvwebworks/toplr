65c6ae84ab68298e22a7f985d3c862df4dad9e81
Marcos <vmvwebworks@gmail.com>
2018-07-23 12:31:59 +0200

    solved devise without email for tumblr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:tumblr]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :omniauthable, omniauth_providers: [:tumblr]
   def self.from_omniauth(access_token)
     data = access_token.info
-    puts data
-    where(provider: access_token['provider'], uid: access_token['uid']).first_or_create! do |user|
-      user.email = auth.info.email
+    puts access_token.to_yaml
+    find_or_create_by(provider: access_token['provider'], uid: access_token['uid']) do |user|
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image
+      user.name = data.name   # assuming the user model has a name
+      user.image = data.image
     end
+  end
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_185107) do
     t.string "provider"
     t.string "name"
     t.string "image"
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
@@ -29,8 +25,6 @@ ActiveRecord::Schema.define(version: 2018_07_19_185107) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
